70d806cf68ef20f5bbd931a4c1c9f8e1d7d9fa91
Juan Martin Raimondo <juanmarraimondo@gmail.com>
2026-05-06 12:50:35 -0300

    Commit inicial. Se sube la estructura del proyecto

---
+++ src/main/java/com/raimondo/Eventracker/EventrackerApplication.java
@@ -0,0 +1,13 @@
+package com.raimondo.Eventracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raimondo/Eventracker/EventrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raimondo.Eventracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
